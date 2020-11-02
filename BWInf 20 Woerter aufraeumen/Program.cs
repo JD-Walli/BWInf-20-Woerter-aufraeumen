a5eb4c767cd8a9df74806327cd22ae5bccb39e87
JD_Walli <jakov@wazungu.eu>
2020-11-02 12:52:02 +0100

    Projektdateien hinzufügen.

---
+++ BWInf 20 Woerter aufraeumen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWInf_20_Woerter_aufraeumen {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
