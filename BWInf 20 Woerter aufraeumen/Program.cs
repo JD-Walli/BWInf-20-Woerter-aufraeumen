f132e5835151f6097b1c453c7eed0bea8aa93bf1
JD_Walli <jakov@wazungu.eu>
2020-11-07 15:31:55 +0100

    minor changes

--- BWInf 20 Woerter aufraeumen/Program.cs
+++ BWInf 20 Woerter aufraeumen/Program.cs
@@ -9,7 +9,7 @@ namespace BWInf_20_Woerter_aufraeumen {
     class Program {
         static void Main(string[] args) {
             Console.WriteLine("\nLösung:");
-            Console.WriteLine("\n" + findSolution(readFile(@"C:\Users\Jakov\Desktop\git\BWInf 20\BWInf 20 Woerter aufraeumen\raetsel4.txt")));
+            Console.WriteLine("\n" + findSolution(readFile(@"C:\Users\Jakov\Desktop\git\BWInf 20\BWInf 20 Woerter aufraeumen\raetsel3.txt")));
             Console.ReadKey();
         }
 
@@ -95,80 +95,82 @@ namespace BWInf_20_Woerter_aufraeumen {
                 }
             }
 
-            float[,] matrix = new float[matrixlength, matrixlength];
-            int x = 0, y = 0;
-            for (int wort1 = 0; wort1 < lueckenPaare.Count; wort1++) {
-                if (lueckenPaare[wort1].passtList.Count > 1) {
-                    for (int passt1ind = 0; passt1ind < lueckenPaare[wort1].passtList.Count; passt1ind++) {
-                        if (unbenutzteWoerter[lueckenPaare[wort1].passtList[passt1ind]] != "") {
-                            for (int wort2 = 0; wort2 < lueckenPaare.Count; wort2++) {
-                                if (lueckenPaare[wort2].passtList.Count > 1) {
-                                    for (int passt2ind = 0; passt2ind < lueckenPaare[wort2].passtList.Count; passt2ind++) {
-                                        if (unbenutzteWoerter[lueckenPaare[wort2].passtList[passt2ind]] != "") {
-                                            int passt1 = lueckenPaare[wort1].passtList[passt1ind];
-                                            int passt2 = lueckenPaare[wort2].passtList[passt2ind];
-
-                                            if (wort1 == wort2) {
-                                                if (passt1 == passt2) { matrix[x, y] = -2; } //grundbelohnung
-                                                else { matrix[x, y] = 2; } //bestrafung: nur ein passt pro wort
-                                            }
-
-                                            if (passt1 == passt2 && wort1 != wort2) {
-                                                matrix[x, y] = 2; //bestrafung jedes passt nur einmal
-                                            }
-
-                                            y++;
-                                        }
-                                    }
-                                }
-                            }
-                            Console.WriteLine(x + " = ({0} , {1})", wort1, lueckenPaare[wort1].passtList[passt1ind]);
-                            x++;
-                            y = 0;
-                        }
-                    }
-                }
-            }
-
-            Matrix.printMatrix(matrix);
-            qaConstellation constellation;
-            try {
-                Dictionary<string, string> qaArguments = new Dictionary<string, string>() {
-                {"annealing_time","30"},
-                {"num_reads","1000"}, //max 10000 (limitation by dwave)
-                {"chain_strength","1.7" }
-                };
-                Dictionary<string, string> pyParams = new Dictionary<string, string>() {
-                {"problem_type","qubo"}, //qubo //ising
-                {"dwave_solver", "DW_2000Q_6"}, //DW_2000Q_6 //Advantage_system1.1
-                {"dwave_inspector","false" }
-                };
-                Task<qaConstellation> constellationTask = QA_Classification.Program.qaCommunication(matrix, qaArguments, pyParams);
-                constellation = constellationTask.Result;
-                constellation.printConstellation();
-                QA_Classification.Program.getUserInput(constellation, matrix);
-
-                int[] solution = constellation.results[constellation.getLowest(1)].Item4;
-                int c = 0;
+            if (matrixlength > 0) {
+                float[,] matrix = new float[matrixlength, matrixlength];
+                int x = 0, y = 0;
                 for (int wort1 = 0; wort1 < lueckenPaare.Count; wort1++) {
                     if (lueckenPaare[wort1].passtList.Count > 1) {
                         for (int passt1ind = 0; passt1ind < lueckenPaare[wort1].passtList.Count; passt1ind++) {
                             if (unbenutzteWoerter[lueckenPaare[wort1].passtList[passt1ind]] != "") {
-                                if (solution[c] == 1) {
-                                    lueckenPaare[wort1].passt = lueckenPaare[wort1].passtList[passt1ind];
+                                for (int wort2 = 0; wort2 < lueckenPaare.Count; wort2++) {
+                                    if (lueckenPaare[wort2].passtList.Count > 1) {
+                                        for (int passt2ind = 0; passt2ind < lueckenPaare[wort2].passtList.Count; passt2ind++) {
+                                            if (unbenutzteWoerter[lueckenPaare[wort2].passtList[passt2ind]] != "") {
+                                                int passt1 = lueckenPaare[wort1].passtList[passt1ind];
+                                                int passt2 = lueckenPaare[wort2].passtList[passt2ind];
+
+                                                if (wort1 == wort2) {
+                                                    if (passt1 == passt2) { matrix[x, y] = -2; } //grundbelohnung
+                                                    else { matrix[x, y] = 2; } //bestrafung: nur ein passt pro wort
+                                                }
+
+                                                if (passt1 == passt2 && wort1 != wort2) {
+                                                    matrix[x, y] = 2; //bestrafung jedes passt nur einmal
+                                                }
+
+                                                y++;
+                                            }
+                                        }
+                                    }
                                 }
-                                c++;
+                                Console.WriteLine(x + " = ({0} , {1})", wort1, lueckenPaare[wort1].passtList[passt1ind]);
+                                x++;
+                                y = 0;
                             }
                         }
                     }
                 }
+
+
+                Matrix.printMatrix(matrix);
+                qaConstellation constellation;
+                try {
+                    Dictionary<string, string> qaArguments = new Dictionary<string, string>() {
+                {"annealing_time","30"},
+                {"num_reads","1000"}, //max 10000 (limitation by dwave)
+                {"chain_strength","1.7" }
+                };
+                    Dictionary<string, string> pyParams = new Dictionary<string, string>() {
+                {"problem_type","qubo"}, //qubo //ising
+                {"dwave_solver", "DW_2000Q_6"}, //DW_2000Q_6 //Advantage_system1.1
+                {"dwave_inspector","false" }
+                };
+                    Task<qaConstellation> constellationTask = QA_Classification.Program.qaCommunication(matrix, qaArguments, pyParams);
+                    constellation = constellationTask.Result;
+                    constellation.printConstellation();
+                    QA_Classification.Program.getUserInput(constellation, matrix);
+
+                    int[] solution = constellation.results[constellation.getLowest(1)].Item4;
+                    int c = 0;
+                    for (int wort1 = 0; wort1 < lueckenPaare.Count; wort1++) {
+                        if (lueckenPaare[wort1].passtList.Count > 1) {
+                            for (int passt1ind = 0; passt1ind < lueckenPaare[wort1].passtList.Count; passt1ind++) {
+                                if (unbenutzteWoerter[lueckenPaare[wort1].passtList[passt1ind]] != "") {
+                                    if (solution[c] == 1) {
+                                        lueckenPaare[wort1].passt = lueckenPaare[wort1].passtList[passt1ind];
+                                    }
+                                    c++;
+                                }
+                            }
+                        }
+                    }
+                }
+                catch (Exception e) {
+                    Console.WriteLine("\nERROR occured:");
+                    Console.WriteLine(e.Message);
+                    Console.WriteLine(e.StackTrace);
+                }
             }
-            catch (Exception e) {
-                Console.WriteLine("\nERROR occured:");
-                Console.WriteLine(e.Message);
-                Console.WriteLine(e.StackTrace);
-            }
-            
             //leere auffüllen
             for (int i = 0; i < lueckenPaareLeer.Count; i++) {
                 for (int k = 0; k < lueckenPaareLeer[i].passtList.Count; k++) {
@@ -189,7 +191,7 @@ namespace BWInf_20_Woerter_aufraeumen {
             return string.Join(" ", originalText);
         }
 
-        public static Tuple<string[], List<Luecke>, string> readFile(string pfad = @"C:\Users\Jakov\Desktop\git\BWInf 20\BWInf 20 Woerter aufraeumen\raetsel0.txt") {
+        public static Tuple<string[], List<Luecke>, string> readFile(string pfad = @"C:\Users\Jakov\Desktop\git\BWInf 20\BWInf 20 Woerter aufraeumen\raetsel1txt") {
             string[] lines = new string[2];
             string line = "";
             System.IO.StreamReader file = new System.IO.StreamReader(pfad);
@@ -202,6 +204,8 @@ namespace BWInf_20_Woerter_aufraeumen {
             string[] woerter = lines[1].Split(' ');
             Console.WriteLine("Wörter: ");
             Console.WriteLine(string.Join(", ", woerter));
+            Console.WriteLine("Aufgabe:");
+            Console.WriteLine(lines0out);
             List<Luecke> lueckenPaare = new List<Luecke>();
             while (lines[0].Contains(",")) {
                 lines[0] = lines[0].Remove(lines[0].IndexOf(","), 1);
